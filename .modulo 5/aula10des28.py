25fccec1b093abf81a559e550f5d4c1b58e82e42
Fhilipe Vendrame <fhilipevendrame99@gmail.com>
2026-03-09 22:58:28 -0300

    feat: add exercise aula10des28 - using if/else with package random

---
+++ .modulo 5/aula10des28.py
@@ -0,0 +1,8 @@
+import random
+palpite = int(input('Advinhe o numero que pensei de 0 a 5: '))
+numero = random.randint(0,5)
+print(numero)
+if palpite == numero:
+    print('Você acertou')
+else:
+    print('Você errou')
